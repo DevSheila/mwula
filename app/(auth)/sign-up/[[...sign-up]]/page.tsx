45a69586e8287db5faa6e8a85df8d119d435e35e
DevSheila <devsheilawambui@gmail.com>
2025-06-25 17:54:39 +0300

    Refactor layout structure and enhance sign-in/sign-up pages: - Update layout components to improve structure and readability. - Replace main container styles in layout.tsx for better responsiveness. - Add background images and update text content on sign-in and sign-up pages for enhanced user experience.

--- app/(auth)/sign-up/[[...sign-up]]/page.tsx
+++ app/(auth)/sign-up/[[...sign-up]]/page.tsx
@@ -1,35 +1,64 @@
-import { SignUp, ClerkLoaded, ClerkLoading } from "@clerk/nextjs";
+import { Logo } from "@/components/logo";
+import { SignIn, SignUp, ClerkLoaded, ClerkLoading } from "@clerk/nextjs";
 import { Loader2 } from "lucide-react";
 import Image from "next/image";
 
 export default function Page() {
     return (
-        <div className="min-h-screen grid grid-cols-1 lg:grid-cols-2">
-            <div className="h-full lg:flex flex-col items-center justify-center px-4">
-                <div className="text-center space-y-4 pt-16">
-                    <h1 className="font-bold text-3xl text-[#2E2A47] dark:text-white">
-                        Welcome to Mwula
-                    </h1>
-                    <p className="text-base text-[#7E8CA0]">
-                        Create your account to get started
+        <div className="lg:grid lg:min-h-screen lg:grid-cols-12">
+            <section className="relative flex h-32 items-end bg-blue-900 lg:col-span-5 lg:h-full xl:col-span-6">
+                <img
+                    alt=""
+                    src="/auth/signup.jpg"
+                    className="absolute inset-0 h-full w-full object-cover opacity-80"
+                />
+
+                <div className="hidden lg:relative lg:block lg:p-12">
+                    <a className="block text-white" href="#">
+                        <span className="sr-only">Home</span>
+                    </a>
+
+                    <h2 className="mt-6 text-2xl font-bold text-white sm:text-3xl md:text-4xl">
+                        Welcome  To Mwula.
+                    </h2>
+
+                    <p className="mt-4 leading-relaxed text-white/90">
+                        Take full control of your finances with Mwula. Start managing your money smarter, saving better, and reaching your goals faster.
                     </p>
-                    <div className="flex items-center justify-center mt-8">
-                        <ClerkLoaded>
-                            <SignUp path="/sign-up" />
-                        </ClerkLoaded>
-                        <ClerkLoading>
-                            <Loader2 className="animate-spin text-muted-foreground" />
-                        </ClerkLoading>
+                </div>
+            </section>
+
+            <div className="flex items-center justify-center px-8 py-8 sm:px-12 lg:col-span-7 lg:px-16 lg:py-12 xl:col-span-6">
+                <div className="max-w-xl lg:max-w-3xl">
+                    <div className="relative -mt-16 block lg:hidden">
+                        <a
+                            className="inline-flex size-16 items-center justify-center rounded-full bg-white text-blue-600 sm:size-20"
+                            href="/"
+                        >
+                            <span className="sr-only">Home</span>
+
+                            <Logo className="text-[#0004E8] dark:text-white" size={28} />
+
+                        </a>
+
+                        <h1 className="mt-2 text-lg font-bold  sm:text-3xl md:text-4xl">
+                            Welcome  To Mwula
+                        </h1>
+
+
                     </div>
+
+                    <ClerkLoaded>
+                        <SignUp path="/sign-up" />
+                    </ClerkLoaded>
+                    <ClerkLoading>
+                        <Loader2 className="animate-spin text-muted-foreground" />
+                    </ClerkLoading>
                 </div>
             </div>
-            <div className="bg-muted relative hidden lg:block">
-                <img
-                    src="/auth/signup.jpg"
-                    alt="Image"
-                    className="absolute inset-0 h-full w-full object-cover dark:brightness-[0.2] dark:grayscale"
-                />
-            </div>
+
+
         </div>
+
     );
 }

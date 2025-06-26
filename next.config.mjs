11e7b414c69a613a82f77cc3b8a762e61b728ef2
DevSheila <devsheilawambui@gmail.com>
2025-06-26 09:33:51 +0300

    Integrate Next.js Bundle Analyzer for improved build insights:
    - Add @next/bundle-analyzer to the project dependencies for analyzing bundle sizes.
    - Update next.config.mjs to enable bundle analysis based on the ANALYZE environment variable.

--- next.config.mjs
+++ next.config.mjs
@@ -1,14 +1,17 @@
+import bundleAnalyzer from '@next/bundle-analyzer';
+
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-      typescript: {
+const withBundleAnalyzer = bundleAnalyzer({
+  enabled: process.env.ANALYZE === 'true',
+});
+
+const nextConfig = withBundleAnalyzer({
+  typescript: {
     ignoreBuildErrors: true,
   },
   eslint: {
     ignoreDuringBuilds: true,
   },
-};
+});
 
 export default nextConfig;
-
-
-
